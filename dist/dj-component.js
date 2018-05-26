d45e9c8d085d33bb690480e5f79637a7600fa1df
linjianhong <linjianhong@github.com>
2018-05-26 09:47:51 +0800

    tag v0.1.16

--- dist/dj-component.js
+++ dist/dj-component.js
@@ -3001,6 +3001,7 @@ angular.module('dj-form').filter('formFormat', function () {
       maxCount: "<",
       imgs: "<",
       mode: '@',
+      onChange: "&",
       updateImg: "&" //选择图片更新用的回调函数
     },
     controller: ["$scope", "$http", "IMG", "DjPop", ctrl]
@@ -3015,11 +3016,7 @@ angular.module('dj-form').filter('formFormat', function () {
     this.$onInit = function () {};
     this.$onChanges = function (changes) {
       if (changes.imgs) {
-        var imgs = changes.imgs.currentValue || [];
-        $scope.imgList.splice(0, $scope.imgList.length);
-        imgs.map(function (url) {
-          return $scope.imgList.push(url);
-        });
+        $scope.imgList = angular.merge([], changes.imgs.currentValue || []);
       }
       if (changes.maxCount) {
         $scope.maxCount = +changes.maxCount.currentValue || 9;
@@ -3033,9 +3030,12 @@ angular.module('dj-form').filter('formFormat', function () {
       if (n < 0 || n >= $scope.imgList.length) return;
       return DjPop.confirm("您确认要删除当前图片?").then(function (a) {
         imgs.splice(n, 1);
-        $scope.imgList = angular.merge([], imgs);;
+        $scope.imgList = angular.merge([], imgs);
+      }).then(function () {
         //console.log("删除图片", $scope.imgList);
-        _this16.updateImg({ imgs: $scope.imgList });
+        var imgs = angular.merge([], $scope.imgList);
+        _this16.updateImg({ imgs: imgs, value: imgs });
+        _this16.onChange({ imgs: imgs, value: imgs });
       });
     };
     $scope.clickImg = function (n) {
@@ -3050,11 +3050,13 @@ angular.module('dj-form').filter('formFormat', function () {
         console.log("EEE", data);
       });
     };
-    this.addImg = function (fn) {
+    this.addImg = function (url) {
       if ($scope.imgList.length >= $scope.maxCount) return;
-      $scope.imgList.push(fn);
+      $scope.imgList.push(url);
       //console.log("添加图片", $scope.imgList);
-      _this16.updateImg({ imgs: $scope.imgList });
+      var imgs = angular.merge([], $scope.imgList);
+      _this16.updateImg({ imgs: imgs, value: imgs });
+      _this16.onChange({ imgs: imgs, value: imgs });
     };
 
     /**
@@ -3125,3 +3127,159 @@ angular.module('dj-form').filter('formFormat', function () {
     };
   }
 })();
+
+/**
+ * 左滑，显示删除按钮
+ * 依赖：angular-touch, flex css
+ * 要求：使用本指令的元素显示为一行无补白，且只有一个下级元素来显示正常内容
+ * 使用：
+      <div left-swipt-delete="deleteItem($index)">
+        <div>显示内容</div>
+      </div>
+
+
+      $scope.deleteItem = function(index) {
+        // do something here
+        // return false; // close the botton animate
+        // return value; // value!==false, close the botton quickly
+        // return $http.post(...); // close the botton when promised, reject as false
+      }
+ */
+angular.module('ngTouch').directive("leftSwiptDelete", ['$parse', '$compile', '$swipe', '$q', function ($parse, $compile, $swipe, $q) {
+  // the button text
+  var BUTTON_TEXT = '删除';
+  // The maximum vertical delta for a swipe should be less than 75px.
+  var MAX_VERTICAL_DISTANCE = 75;
+  // Vertical distance should not be more than a fraction of the horizontal distance.
+  var MAX_VERTICAL_RATIO = 0.3;
+  // At least a 30px lateral motion is necessary for a swipe.
+  var MIN_HORIZONTAL_DISTANCE = 30;
+  // the delete button width in px
+  var BUTTON_WIDTH = 100;
+
+  return function (scope, element, attr) {
+    var theAttr = attr['leftSwiptDelete'];
+    var swipeHandler = $parse(theAttr);
+
+    var startCoords,
+        valid,
+        theBtn,
+        opening = false;
+
+    function validSwipe(coords) {
+      // Check that it's within the coordinates.
+      // Absolute vertical distance must be within tolerances.
+      // Horizontal distance, we take the current X - the starting X.
+      // This is negative for leftward swipes and positive for rightward swipes.
+      // After multiplying by the direction (-1 for left, +1 for right), legal swipes
+      // (ie. same direction as the directive wants) will have a positive delta and
+      // illegal ones a negative delta.
+      // Therefore this delta must be positive, and larger than the minimum.
+      if (!startCoords) return false;
+      var deltaY = Math.abs(coords.y - startCoords.y);
+      var deltaX = coords.x - startCoords.x;
+      var moved = opening ? deltaX > MIN_HORIZONTAL_DISTANCE : -deltaX > MIN_HORIZONTAL_DISTANCE;
+      return valid && // Short circuit for already-invalidated swipes.
+      deltaY < MAX_VERTICAL_DISTANCE && deltaY / Math.abs(deltaX) < MAX_VERTICAL_RATIO && moved;
+    }
+
+    var pointerTypes = ['touch'];
+    if (!angular.isDefined(attr['ngSwipeDisableMouse'])) {
+      pointerTypes.push('mouse');
+    }
+    var theElement;
+    setTimeout(function () {
+      theElement = showMoving.element = element.children().eq(0);
+      theElement.css({
+        "position": 'relative',
+        "z-index": "2"
+      });
+      bind(theElement);
+      initButton();
+    });
+    function bind(element) {
+      $swipe.bind(element, {
+        'start': function start(coords, event) {
+          startCoords = coords;
+          valid = true;
+        },
+        'move': function move(coords, event) {
+          var dx = coords.x - startCoords.x - (opening ? BUTTON_WIDTH : 0);
+          if (dx > 0) dx = 0;
+          showMoving(dx);
+        },
+        'cancel': function cancel(event) {
+          valid = false;
+          showMoving(0);
+        },
+        'end': function end(coords, event) {
+          opening = validSwipe(coords) ? !opening : opening;
+          showMoving(opening ? "show" : "hide");
+        }
+      }, pointerTypes);
+      angular.element(element).on("click", function () {
+        showMoving("hide");
+      });
+    }
+
+    function initButton() {
+      element.css("position", 'relative');
+      var template = '<div class="left-swipe-delete-btn flex-cc">' + BUTTON_TEXT + '</div>';
+      theBtn = $compile(template)(scope);
+      element.append(theBtn);
+      theBtn.bind("click", function (event) {
+        var result = swipeHandler(scope, { $event: event });
+        if (result === false) {
+          showMoving("hide");
+          return;
+        }
+        $q.when(result).then(function () {
+          showMoving(0);
+          opening = false;
+        }).catch(function () {
+          showMoving("hide");
+        });
+      });
+    }
+
+    var showMoving = function showMoving(dx) {
+      if (!showMoving.element) {
+        showMoving.element = element.children().eq(0);
+        showMoving.element.css("display", 'block');
+      }
+      if (dx == "show") {
+        showMoving.element.css("transition-duration", "0.5s");
+        showMoving.element.css("transform", 'translateX(-' + BUTTON_WIDTH + 'px)');
+        autoclose();
+        opening = true;
+      } else if (dx == "hide") {
+        showMoving.element.css("transition-duration", "0.5s");
+        showMoving.element.css("transform", 'translateX(0)');
+        opening = false;
+      } else {
+        showMoving.element.css("transition-duration", "0s");
+        showMoving.element.css("transform", 'translateX(' + dx + 'px)');
+      }
+    };
+
+    function insideElement(target, ele) {
+      if (!target || !ele) return false;
+      if (target == ele[0] || target == ele) return true;
+      return insideElement(target.parentElement, ele);
+    }
+
+    function autoclose() {
+      var close = function close(event) {
+        document.removeEventListener("mousedown", close, false);
+        document.removeEventListener("touchstart", close, false);
+        var target = event.target || event.srcElement;
+        if (insideElement(target, theBtn) || insideElement(target, showMoving.element)) {
+          return;
+        }
+        showMoving("hide");
+      };
+      document.addEventListener("mousedown", close, false);
+      document.addEventListener("touchstart", close, false);
+    }
+  };
+}]);
